9e212418c6248c3ba4935003b091b2a01d813a4f
Manish Maurya <askmanishmaurya@gmail.com>
2026-04-18 20:23:46 +0400

    removing TF block from main.tf

--- modules/aws-eks-nodegroup/main.tf
+++ modules/aws-eks-nodegroup/main.tf
@@ -15,15 +15,6 @@
  * EKS clusters across AWS US-East, EU-West, and AP-South regions.
  */
 
-terraform {
-  required_version = ">= 1.5.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 5.0.0, < 6.0.0"
-    }
-  }
-}
 
 locals {
   # Mandatory tags applied to every AWS resource.
